ccabceafdc8b963f31951bb72f93965a0a180fdd
luanaassisi <luanaaraujodeassis@outlook.com>
2022-05-25 19:19:38 -0300

    Js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 
-$("#enviar").click(() => {
+$("#enviar").click(function (e) {
+    e.preventDefault();
     let nasa = $("#nasaDate").val();
     console.log(nasa);
     $.ajax({
@@ -8,7 +9,17 @@ $("#enviar").click(() => {
         success: function (obj) {
             console.log(obj);
             $("#titulo").text(obj.title);
-            $("#img").attr("src", obj.media_type);
+            $("#img").attr("src", obj.url);
+            //if (media_type == "imagem") {
+            //   $("#video").css("display", "none")
+            //    $("#img").css("display", "block")
+            //    $("#img").attr("src", obj.url);
+            // } else {
+            //    $("#img").css("display", "none")
+            //    $("#video").css("display", "block")
+            //    $("#video").attr("src", obj.url);
+
+            //}
             $("#descricao").text(obj.explanation);
             $("#data").date(obj.date);
         },
@@ -17,6 +28,7 @@ $("#enviar").click(() => {
             $("#img").attr("src", "");
             $("#descricao").text("erro");
             $("#data").text("erro");
+            $("#video").attr("src", "");
 
         },
 
